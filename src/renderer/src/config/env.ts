6cc4aeca17fa71b5749a485ea489570388878b8f
wantnaM <1178862455@qq.com>
2025-03-16 12:25:39 +0800

    修复问题

--- src/renderer/src/config/env.ts
+++ src/renderer/src/config/env.ts
@@ -3,5 +3,5 @@ export { default as AppLogo } from '@renderer/assets/images/logo.png'
 
 export const APP_NAME = '教师智能体'
 export const isLocalAi = false
-// export const ADMIN_API_URL = 'http://8.134.23.119:8080'
-export const ADMIN_API_URL = 'http://127.0.0.1:48080'
+export const ADMIN_API_URL = 'http://8.134.23.119:8080'
+// export const ADMIN_API_URL = 'http://127.0.0.1:48080'
