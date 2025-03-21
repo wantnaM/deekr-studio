ae6dd080406bf11945c3d5e58cbd30012ab83177
wantnaM <1178862455@qq.com>
2025-03-21 17:20:54 +0800

    删除console.log

--- src/renderer/src/services/AssistantService.ts
+++ src/renderer/src/services/AssistantService.ts
@@ -70,7 +70,6 @@ export function getTranslateModel() {
 
 export function getAssistantProvider(assistant: Assistant): Provider {
   const providers = store.getState().llm.providers
-  console.log(assistant, providers)
   const provider = providers.find((p) => p.id === assistant.model?.provider)
   return provider || getDefaultProvider()
 }
